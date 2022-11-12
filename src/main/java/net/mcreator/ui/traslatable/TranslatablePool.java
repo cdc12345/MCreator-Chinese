c2230118c184d2e7d3f810fc06470843ad3e8d6f
cdc12345 <3154934427@qq.com>
2022-11-12 21:08:37 +0800

    汉化池修复

--- src/main/java/net/mcreator/ui/traslatable/TranslatablePool.java
+++ src/main/java/net/mcreator/ui/traslatable/TranslatablePool.java
@@ -57,11 +57,11 @@ public class TranslatablePool {
 
 	private TranslatablePool() {
 		InputStream defaultPoolInput = null;
-/*		File pool = new File(UserFolderManager.getCacheFolder(),"pools.tra");
+		File pool = new File(UserFolderManager.getCacheFolder(),"pools.tra");
 		if (MCreatorApplication.isInternet) {
 			try {
 				URL url = new URL(
-						"https://ghproxy.com/https://raw.githubusercontent.com/cdc12345/MCreator-Chinese/master/src/main/resources/pools.tra");
+						"https://ghproxy.com/https://raw.githubusercontent.com/cdc12345/MCreator-Chinese/main/pools.tra");
 				logger.info("系统正在与在线翻译池沟通");
 				defaultPoolInput = url.openStream();
 			} catch (IOException e) {
@@ -73,11 +73,11 @@ public class TranslatablePool {
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
 			}
-		}*/
+		}
 		json = new Gson().fromJson(new InputStreamReader(
 				Objects.requireNonNullElse(defaultPoolInput,this.getClass().getResourceAsStream("/pools.tra"))),
 				JsonObject.class);
-//		FileIO.writeStringToFile(json.toString(),pool);
+		FileIO.writeStringToFile(json.toString(),pool);
 		logger.info("翻译池已经准备就绪");
 	}
 	public boolean containValue(String key){
